56b653b09938148666506634c5dfe976dc25d3f9
Imafidon <jimafidon@tcco.com>
2025-09-14 16:39:47 -0500

    Created the foundation of the calendar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+        <h1>TimeTabled 🥚</h1>
+        <h2>*Write message for schedule recipient: unsure right now*</h2>
+        <Calendar />
+    </div>
+  )
+}
+
+export default App
